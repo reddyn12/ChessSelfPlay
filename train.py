54a2b99e598cd1dd7fbfb5a69ddef5e139bb949f
reddyn <nikidsniper@gmail.com>
2023-11-29 18:24:35 -0500

    code runs through, no errors... yet

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 
 
 # %%
-from turtle import up
 from model import Tranformer, GPTConfig, ChessGPT, cross_entropy_loss
 import os
 import jax
@@ -21,7 +20,7 @@ randKEY = jax.random.PRNGKey(seed=123)
 CONTEXT_LENGTH = tokenizer.MAX_MOVES*3+1
 def makeTargets(x):
     data = x[:, :-1]
-    target = x[:, -1:]
+    target = x[:, -1]
     return data, target
 def getBatch(games, size = 10, clip = None):
     # k = jax.random.PRNGKey(0)
@@ -31,10 +30,11 @@ def getBatch(games, size = 10, clip = None):
     idx = jax.random.randint(k, (size,), 0, len(games))
     batch = jnp.take(games, idx, axis=0)
     min_length = jnp.min(jnp.sum(batch != 0, axis = 1))
-    print("Min Length", min_length, games.shape, batch.shape)
+    # print("Min Length", min_length, games.shape, batch.shape)
     randKEY, k = jax.random.split(randKEY)
     randInd = jax.random.randint(k, (1,), 2, min_length)
-    print("Get Batch RAndom",randInd)
+    print("Current Batch Min Length", min_length, "Random Slice @:", randInd)
+    # print("Get Batch RAndom",randInd)
     batch = batch[:, :randInd[0]]
     return batch
 
@@ -46,31 +46,19 @@ games = open('data/ELO_2000_UCI.txt', 'r').read()
 games = games.splitlines()
 
  
-# games = games[:30]
+games = games[:3000]
 
 
 tokenizedGames = []
 
 for g in games:
     arr = jnp.array(tokenizer.tokenizeLine(g, vocab), dtype=jnp.int32)
-    # arr = tokenizer.tokenizeLine(g, vocab)
-
-
-    # arr = arr[:random.randint(1, len(arr)-1)]
     tokenizedGames.append(arr)
     # tokenizedGames = jnp.vstack((tokenizedGames, arr))
     
 print("Converting to jnp array")
 JtokenizedGames = tokenizer.pad_sequences(tokenizedGames, vocab['<PAD>'])
-
 print("FINISHED converting to jnp array")
-
-
-# # %%
-# # FASTEST JAX APPEND
-# JtokenizedGames = jnp.vstack(paddedGames, dtype=jnp.int32)
-
-
 
 config = GPTConfig()
 config.vocab_size = vocabSize
@@ -92,63 +80,42 @@ params = chessModel.init(k, JtokenizedGames[:2])
 
 # %%
 # jnp.save('ELO_2000_UCI_Token.npy', JtokenizedGames)
-def getLossEstimate(size=100):
-    b = getBatch(JtokenizedGames, size)
-    d,t = makeTargets(b)
+def getLoss(params, d, t):
+    # b = getBatch(JtokenizedGames, size)
+    # d,t = makeTargets(b)
     logits= chessModel.apply(params, d)
+    logits = logits[:, -1, :]
 
-    loss = cross_entropy_loss(logits, t) 
+    t_one_hot = jax.nn.one_hot(t, config.vocab_size)
+    # logits = jnp.argmax(logits, axis=-1)
+    print("Logits", logits.shape, "Y:",t.shape)
+
+    loss = optax.softmax_cross_entropy(logits, t_one_hot)
+    loss = jnp.mean(loss)
     return loss
 
 # %%
 nBatches = 1000
 losses = []
-# for i in range(nBatches):
-#     b = getBatch(JtokenizedGames, 10)
-#     d,t = makeTargets(b)
-#     logits, loss = chessModel.apply(params, d, t)
-#     ans = logits[:, -1]
-#     ansTokens = jnp.argmax(ans, axis=-1)
-#     print(i, " | Loss", loss)
-# import jax.numpy as jnp
-
-# # ...
-
 # Create the Adam optimizer
 optimizer = optax.adam(learning_rate=1e-3)
-
-# Define the update function\
-def buildUpdateJit(optimizer):
-    
-    @jax.jit
-    def update(params, opt_state, batch):
-        d,t = makeTargets(batch)
-        logits = chessModel.apply(params, d)
-    
-        loss, grads = jax.value_and_grad(cross_entropy_loss)(logits, t)
-        updates, new_opt_state = optimizer.update(grads, opt_state)
-        
-        new_params = optax.apply_updates(params, updates)
-        return new_params, new_opt_state, loss
-    return update
-# ...
-update = buildUpdateJit(optimizer)
-# Initialize the optimizer state
 opt_state = optimizer.init(params)
 
-
-# ...
 
 # Training loop
 nBatches = 1000
 losses = []
 for i in tqdm(range(nBatches)):
     b = getBatch(JtokenizedGames, 100)
+    d,t = makeTargets(b)
+    loss, grads = jax.value_and_grad(getLoss)(params, d, t)
+    updates, opt_state = optimizer.update(grads, opt_state)
+    params = optax.apply_updates(params, updates)
     # d, t = makeTargets(b)
     # logits= chessModel.apply(params, d)
     # loss, grads = jax.value_and_grad(cross_entropy_loss)(logits, t)
-    print(opt_state)
-    params, opt_state, loss = update(params, opt_state, b)
+    # print(opt_state)
+    # params, opt_state, loss = update(params, opt_state, b)
     # ans = logits[:, -1]
     # ansTokens = jnp.argmax(ans, axis=-1)
     print(i, " | Loss", loss)
