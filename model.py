ca508feda9d8f8d34fb0dc0b980b37fe2b1f348c
reddyn <nikidsniper@gmail.com>
2023-11-30 23:40:20 -0500

    dvs

--- model.py
+++ model.py
@@ -155,47 +155,47 @@ class Tranformer(nn.Module):
             loss = None
         # x = self.lm_head(x)
         return x, loss
-def cross_entropy_loss(logits, labels):
-    log_probs = jax.nn.log_softmax(logits)
-    return -jnp.mean(jnp.take_along_axis(log_probs, labels[:, None], axis=-1))
+# def cross_entropy_loss(logits, labels):
+#     log_probs = jax.nn.log_softmax(logits)
+#     return -jnp.mean(jnp.take_along_axis(log_probs, labels[:, None], axis=-1))
 
 
 
 
-class ChessGPT(nn.Module):
-    def __init__(self, config: GPTConfig):
-        self.config = config
+# class ChessGPT(nn.Module):
+#     def __init__(self, config: GPTConfig):
+#         self.config = config
     
-    def setup(self):
-        nn.Embed()
-        pass
+#     def setup(self):
+#         nn.Embed()
+#         pass
         
-    def __call__(self, x):
-        pass
+#     def __call__(self, x):
+#         pass
 
-class ChessTransformerRANDOM(nn.Module):
-    def setup(self):
-        print("SETUP")
-        self.embed = nn.Embed(
-            num_embeddings=100,
-            features=512,
-        )
-        self.transformer = nn.Transformer(
-            num_encoder_layers=6,
-            num_decoder_layers=6,
-            d_model=512,
-            nhead=8,
-            dim_feedforward=2048,
-            dropout=0.1,
-            activation=nn.gelu,
-        )
-        self.head = nn.Dense(features=100)
+# class ChessTransformerRANDOM(nn.Module):
+#     def setup(self):
+#         print("SETUP")
+#         self.embed = nn.Embed(
+#             num_embeddings=100,
+#             features=512,
+#         )
+#         self.transformer = nn.Transformer(
+#             num_encoder_layers=6,
+#             num_decoder_layers=6,
+#             d_model=512,
+#             nhead=8,
+#             dim_feedforward=2048,
+#             dropout=0.1,
+#             activation=nn.gelu,
+#         )
+#         self.head = nn.Dense(features=100)
 
-    def __call__(self, x):
-        x = self.embed(x)
-        x = self.transformer(x, x)
-        x = self.head(x)
-        return x
+#     def __call__(self, x):
+#         x = self.embed(x)
+#         x = self.transformer(x, x)
+#         x = self.head(x)
+#         return x
 
     
 # testModel = ChessTransformerRANDOM()
