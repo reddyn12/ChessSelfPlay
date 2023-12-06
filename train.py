945b37dd01f4f32c4c981850c77d228c72d1ab17
reddyn <nikidsniper@gmail.com>
2023-12-06 18:43:10 -0500

    sdfdgf

--- train.py
+++ train.py
@@ -248,7 +248,7 @@ for currStep in tqdm(range(nBatches)):
     # states = jax.pmap(lambda x: x)(state)
     # states = jax_utils.replicate(state, devices=jax.local_devices())
     # states = jax.pmap(lambda x: x)([state,state,state,state])
-    inps = [[rngs[0],state],[rngs[1],state],[rngs[2],state],[rngs[3],state]]
+    inps = jnp.array([[rngs[0],state],[rngs[1],state],[rngs[2],state],[rngs[3],state]])
     gradsP, lossP, accuracyP = jax.pmap(trainStepACC)(inps)
     # states,losses,accuracys = trainStepPmap(rngs, state)
     # states = [train_state.TrainState(*state_tup) for state_tup in states_tups]
