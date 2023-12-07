2bd2a4e35ff20d3e6fd56b5bea6e0e59001aa0b8
reddyn <nikidsniper@gmail.com>
2023-12-06 19:32:15 -0500

    nbvcd

--- train.py
+++ train.py
@@ -28,7 +28,8 @@ PAD_TOKEN = vocab['<PAD>']
 nBatches = 10000
 # DROP TRailing 4 if on A100
 BATCH_SIZE = 128//4//1 #* deviceCnt
-BATCH_ACC = 16//1
+# BATCH_ACC = 16//4
+BATCH_ACC = 16*2
 # BLOCK_SIZE = 400
 BLOCK_SIZE = 512
 CONTEXT_LENGTH = tokenizer.MAX_MOVES*3+1
