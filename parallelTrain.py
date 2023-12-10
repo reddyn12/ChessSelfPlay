7ddb80923e03efe314e3d6c1142880c0e58cbf4d
reddyn <nikidsniper@gmail.com>
2023-12-09 23:21:49 -0500

    rehg

--- parallelTrain.py
+++ parallelTrain.py
@@ -34,9 +34,9 @@ vocab, vocabDecode = tokenizer.makeVocabUCI_SMALL()
 PAD_TOKEN = vocab['<PAD>']
 nBatches = 100000
 # DROP TRailing 4 if on A100
-BATCH_SIZE = 128 #* deviceCnt
+BATCH_SIZE = 128//4//1 #* deviceCnt
 # BATCH_ACC = 16//4
-BATCH_ACC = 16//1
+BATCH_ACC = 16*2
 # BLOCK_SIZE = 400
 BLOCK_SIZE = 512
 CONTEXT_LENGTH = tokenizer.MAX_MOVES*3+1
